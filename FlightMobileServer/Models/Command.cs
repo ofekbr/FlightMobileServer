5a1925866b9ac56cf9b8c0692e81afd13903509f
Yana Molodetsky <yana.molodetsky@live.biu.ac.il>
2020-06-11 18:42:11 +0300

    Fist commit of project

---
+++ FlightMobileServer/Models/Command.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace FlightMobileServer.Models
+{
+    public class Command
+    {
+        [Key]
+        public long Id { set; get; }
+
+        [JsonPropertyName("aileron")]
+        public double Aileron { set; get; }
+
+        [JsonPropertyName("Rudder")]
+        public double Rudder { set; get; }
+
+        [JsonPropertyName("elevator")]
+        public double Elevator { set; get; }
+
+        [JsonPropertyName("throttle")]
+        public double Throttle { set; get; }
+
+    }
+}
