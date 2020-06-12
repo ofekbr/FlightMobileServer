4ad41bda3e18843e3cb1a047faac0b30f431ce3c
shani-b <58752443+shani-b@users.noreply.github.com>
2020-06-12 15:20:06 +0300

    Add files via upload

---
+++ FlightMobileServer/Controllers/CommandController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FlightMobileServer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightMobileServer.Controllers
+{
+    [Route("api/command")]
+    [ApiController]
+    public class CommandController : ControllerBase
+    {
+        private readonly SimulatorManager _simulatorManager;
+
+        public CommandController(SimulatorManager sm)
+        {
+            _simulatorManager = sm;
+        }
+
+        // POST: api/Command
+        [HttpPost]
+        public void Post(CommandController command)
+        {
+            
+            
+        }
+
+    }
+}
