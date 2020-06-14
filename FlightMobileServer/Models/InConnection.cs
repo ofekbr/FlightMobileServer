8ef619875a5fbde72b4a6cdc2da3f86cafbda469
Yana Molodetsky <yana.molodetsky@live.biu.ac.il>
2020-06-14 09:23:47 +0300

    added in and out connection

--- FlightMobileServer/Models/InConnection.cs
+++ FlightMobileServer/Models/InConnection.cs
@@ -10,7 +10,20 @@ namespace FlightMobileServer.Models
     public class InConnection
     {
         private Client client;
+        public void Connect(string ip, int port)
+        {
+            client.Connect(ip, port);
+        }
 
-
+        public void Disconnect()
+        {
+            client.Disconnect();
+        }
+        public string GetPhoto()
+        {
+            client.Write("get ?\r\n");
+            string photo = client.Read();
+            return photo;
+        }
     }
 }
--- FlightMobileServer/Models/OutConnection.cs
+++ FlightMobileServer/Models/OutConnection.cs
@@ -10,6 +10,45 @@ namespace FlightMobileServer.Models
     public class OutConnection
     {
         private Client client;
+        public void Connect(string ip, int port)
+        {
+            client.Connect(ip, port);
+        }
 
+        public void Disconnect()
+        {
+            client.Disconnect();
+        }
+        public void SendInfo(int value, string strVal)
+        {
+            // TODO: check about the ip and port - how to recieve it?
+            client.Connect("127.0.0.1", 1234);
+            string send = "";
+            switch (strVal)
+            {
+                case "throttle":
+                    {
+                        send = "set /controls/engines/current-engine/throttle" + value;
+                        break;
+                    }
+                case "elevator":
+                    {
+                        send = "set /controls/flight/elevator" + value;
+                        break;
+                    }
+                case "rudder":
+                    {
+                        send = "set /controls/flight/rudder" + value;
+                        break;
+                    }
+                case "aileron":
+                    {
+                        send = "set /controls/flight/aileron" + value;
+                        break;
+                    }
+            }
+            client.Write(send);
+            client.Disconnect();
+        }
     }
 }
