59dfc5a92633d7e9954bd7721d15d14a34b45a91
shani-b <58752443+shani-b@users.noreply.github.com>
2020-06-18 17:28:27 +0300

    Add files via upload

--- FlightMobileServer/Startup.cs
+++ FlightMobileServer/Startup.cs
@@ -1,16 +1,16 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net;
+using System.Text;
 using System.Threading.Tasks;
 using FlightMobileServer.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace FlightMobileServer
 {
@@ -38,6 +38,13 @@ namespace FlightMobileServer
                 app.UseDeveloperExceptionPage();
             }
 
+            var rewrite = new RewriteOptions()
+               .AddRewrite("command", "api/command", true)
+               .AddRewrite("disconnect", "api/command", true);
+
+            rewrite.Rules.Add(new CustomRule());
+            app.UseRewriter(rewrite);
+            
             app.UseHttpsRedirection();
 
             app.UseRouting();
@@ -48,6 +55,58 @@ namespace FlightMobileServer
             {
                 endpoints.MapControllers();
             });
+        }
+    }
+
+
+
+    public class CustomRule : Microsoft.AspNetCore.Rewrite.IRule
+    {
+        private InConnection _inConnection;
+        
+        public CustomRule()
+        {
+            _inConnection = new InConnection();
+            //TODO try and catch
+            try
+            {
+                _inConnection.Connect("http://localhost:5000/screenshot");
+            }
+            catch (Exception e)
+            {
+
+                int f = 5;    
+            }
+        }
+        
+        public void ApplyRule(RewriteContext context)
+        {
+            var request = context.HttpContext.Request;
+            var host = request.Host;
+            if (request.Path.Value.Contains("screenshot"))
+            {
+                var result = ContentAction(context);
+                return;
+            } else if (request.Path.Value.Contains("command"))
+            {
+                return;
+            }
+
+
+            var response = context.HttpContext.Response;
+            response.StatusCode = (int)HttpStatusCode.BadRequest;
+            context.Result = RuleResult.EndResponse;
+
+
+        }
+        
+        public async Task<HttpResponse> ContentAction(RewriteContext context)
+        {
+            var response = context.HttpContext.Response;
+            byte[] flightGearImage = Encoding.ASCII.GetBytes( await _inConnection.CreateRequestToServer("GET", "http://localhost:5000/screenshot"));
+            response.ContentType = "screenshot";
+            await response.Body.WriteAsync(flightGearImage, 0, flightGearImage.Length);
+            return response;
         }
     }
 }
